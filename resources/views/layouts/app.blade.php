9f07285211b37e04b7e12eeeb08609fbdb95d55b
David Cheng <chengshair@gmail.com>
2021-02-28 19:44:53 +0800

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="/css/navbar.css">
+        <link rel="stylesheet" href="css/all.css">
+        <link rel="stylesheet" href="semantic-ui/semantic.css">
+
+        {{-- <link rel="stylesheet" href="bootstrap5/bootstrap.min.css"> --}}
+
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            {{-- <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header> --}}
+
+            <!-- Page Content -->
+            <main id="main" style="margin-left:75px;">
+                {{ $slot }}
+            </main>
+        </div>
+        <script src="js/jquery-3.5.1.slim.min.js"></script>
+        <script src="js/all.js" ></script>
+        <script src="semantic-ui/semantic.js" ></script>
+
+        {{-- <script src="bootstrap5/bootstrap.min.js" ></script> --}}
+
+
+        <script>
+            var navOpen = false;
+            function toggleNav(){
+                if(navOpen){
+                    navOpen = false;
+                    $("#nav-btn-arrow").toggleClass('rotated');
+                    document.getElementById("my-side-nav").style.width = "75px";
+                    document.getElementById("main").style.marginLeft= "75px";
+    
+                } else {
+                    navOpen = true;
+                    $("#nav-btn-arrow").toggleClass('rotated');
+                    document.getElementById("my-side-nav").style.width = "250px";
+                    document.getElementById("main").style.marginLeft = "250px";
+                }
+            }
+            $("#logout").click(function(){
+                $("#logout-form").submit();
+            })
+        </script>
+    </body>
+</html>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,54 @@
+
+<div id="my-side-nav" class="sidenav container" style="width:75px; overflow: visible;">
+    <button  class="nav-toggle-btn" onclick="toggleNav()"><i id="nav-btn-arrow" class="fas fa-chevron-right" style="color: gray;"></i></button>
+
+    <div id="nav-header" class="mb-5" style="">
+        <img src="/image/chef-icon-white.png" alt="" style="color:gray; display: block; margin-left: auto; margin-right: auto; width: 50%;">
+    </div>
+    <div id="nav-body" style="overflow-x:hidden;"> 
+        
+        <div>
+            <a href="/dashboard" class="link-container">
+                <i class="fas fa-stopwatch fa-fw"></i>
+                <span class="link-title" >即時報表</span>
+            </a>
+        </div>
+        <div>
+            <a href="/dashboard" class="link-container">
+                <i class="fas fa-chart-area fa-fw"></i>
+                <span class="link-title" >營業報表</span>
+            </a>
+        </div>
+        <div>
+            <a href="/dashboard" class="link-container">
+                <i class="fas fa-burn fa-fw"></i>
+                <span class="link-title" >商品設定</span>
+            </a>
+        </div>
+        <div>
+            <a href="/dashboard" class="link-container">
+                <i class="fas fa-cubes fa-fw"></i>
+                <span class="link-title" >庫存清單</span>
+            </a>
+        </div>
+        
+    </div>
+    <div id="nav-footer" class="container" style="position: absolute; bottom: 1em; color: white;">
+        <div class="dropup" style="width: 100%;">
+            <button class="dropbtn">
+                <i class="fas fa-angle-right"></i>
+            </button>
+            <div class="dropup-content">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <x-responsive-nav-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log out') }}
+                    </x-responsive-nav-link>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</div>
