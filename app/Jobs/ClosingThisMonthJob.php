907fec42366a31252f45e5ac5bf37628076d018f
David Cheng <chengshair@gmail.com>
2021-07-06 13:08:02 +0800

    implement closing year month cron job

---
+++ app/Jobs/ClosingThisMonthJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use DateTime;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+
+class ClosingThisMonthJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    private $closingService;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->closingService = app()->make('ClosingService');
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $nowDateTime = new DateTime("first day of last month");
+        $yearMonth = $nowDateTime -> format('Y-m');
+        $this->closingService->closeMonth($yearMonth);
+    }
+}
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,2 +1,3 @@
 service apache2 restart
+service cron start
 exec /sbin/init
